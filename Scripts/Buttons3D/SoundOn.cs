6690d69624fe2a111696307520c0f065b97ef32b
BoberUnity <borisberez@rambler.ru>
2014-01-06 00:51:50 +0400

    Генератор работает тише, когда отходишь

--- Scripts/Buttons3D/SoundOn.cs
+++ Scripts/Buttons3D/SoundOn.cs
@@ -6,13 +6,28 @@ namespace Assets.Scripts.Buttons3D
   public class SoundOn : Button3DBase
   {
     [SerializeField] private AudioClip sound = null;
+    [SerializeField] private float distanse = 1.5f;
+    [SerializeField] private float addVolume = 0.1f;//Компенсация, т.к. дистанция не м.б. 0
     [SerializeField] private bool loop = false;
+    private Transform t = null;
+    private Transform ct = null;
 
     protected override void MakeAction()
     {
       audio.clip = sound;
       audio.loop = loop;
       audio.Play();
+    }
+
+    private void Start()
+    {
+      t = transform;
+      ct = character.transform;
+    }
+
+    private void Update()
+    {
+      audio.volume = 1 - Vector3.Distance(t.position, ct.position) / distanse + addVolume;
     }
   }
 }
