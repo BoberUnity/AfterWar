4e5876825fa899fa50e23786670e51dfba697216
BoberUnity <borisberez@rambler.ru>
2014-01-07 22:18:51 +0400

    Настройки графики

--- Scripts/GUI/ButtonSettings.cs
+++ Scripts/GUI/ButtonSettings.cs
@@ -15,6 +15,7 @@ public class ButtonSettings : MonoBehaviour
         QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
       if (id == 2)
         QualitySettings.anisotropicFiltering = AnisotropicFiltering.Disable;
+
       if (id == 3)
         QualitySettings.antiAliasing = 0;
       if (id == 4)
@@ -23,6 +24,22 @@ public class ButtonSettings : MonoBehaviour
         QualitySettings.antiAliasing = 4;
       if (id == 6)
         QualitySettings.antiAliasing = 8;
+
+      if (id == 7)
+        QualitySettings.vSyncCount = 0;
+      if (id == 8)
+        QualitySettings.vSyncCount = 1;
+      if (id == 9)
+        QualitySettings.vSyncCount = 2;
+
+      if (id == 10)
+        QualitySettings.masterTextureLimit = 3;
+      if (id == 11)
+        QualitySettings.masterTextureLimit = 2;
+      if (id == 12)
+        QualitySettings.masterTextureLimit = 1;
+      if (id == 13)
+        QualitySettings.masterTextureLimit = 0;
     }
   }
 }
