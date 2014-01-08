d04f5431ce42e66cc9243cf55a6b3d798903450a
BoberUnity <borisberez@rambler.ru>
2014-01-08 23:46:33 +0400

    Settings

---
+++ Scripts/GUI/CallOptions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CallOptions : MonoBehaviour 
+{
+  [SerializeField] private GameObject[] activeObjs = null;
+  [SerializeField] private GameObject[] deactiveObjs = null;
+
+  protected virtual void OnPress(bool isPressed)
+  {
+    if (!isPressed)
+    {
+      foreach (var aObjs in activeObjs)
+      {
+        aObjs.SetActive/*Recursively*/(true);
+      }
+      foreach (var aObjs in deactiveObjs)
+      {
+        aObjs.SetActive/*Recursively*/(false);
+      }
+    }
+	}
+}
