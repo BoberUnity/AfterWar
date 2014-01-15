618123380bdb8fb8822ac2d8b8879877ea0fff05
BoberUnity <borisberez@rambler.ru>
2014-01-16 00:51:44 +0400

    Прыжки по бочкам

--- Scripts/AnimOnTrigger.cs
+++ Scripts/AnimOnTrigger.cs
@@ -5,16 +5,27 @@
 public class AnimOnTrigger : MonoBehaviour
 {
   [SerializeField] private Animation anim = null;
+  private bool played = false;
 
 	private void OnTriggerEnter(Collider other)
   {
-    if (other.gameObject.name == "Stalker")
+    if (other.gameObject.name == "Stalker" && !played)
+    {
       anim.Play();
+      played = true;
+    }
+    if (other.gameObject.name == "Stalker" && played)
+    {
+      //animation[animation.clip.name].time = 1;
+      animation[animation.clip.name].speed = -1;
+      anim.Play();
+      played = false;
+    }
 	}
   //
-  private void OnTriggerExit(Collider other)
-  {
-    if (other.gameObject.name == "Stalker")
-      anim.Play();
-  }
+  //private void OnTriggerExit(Collider other)
+  //{
+  //  if (other.gameObject.name == "Stalker")
+  //    anim.Stop();
+  //}
 }
