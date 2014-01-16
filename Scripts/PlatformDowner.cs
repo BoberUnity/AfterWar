ebbb975f853c515b5142a0c4177dc1b011e7e15f
BoberUnity <borisberez@rambler.ru>
2014-01-17 00:21:55 +0400

    Бочки

--- Scripts/PlatformDowner.cs
+++ Scripts/PlatformDowner.cs
@@ -3,33 +3,38 @@ using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 
-public class AnimOnTrigger : MonoBehaviour
+public class PlatformDowner : MonoBehaviour
 {
   [SerializeField] private Animation anim = null;
   [SerializeField] private AnimationClip forwardClip = null;
   [SerializeField] private AnimationClip backClip = null;
   private bool isPlaying = false;
-  private bool inEnd = false;
 
 	private void OnTriggerEnter(Collider other)
   {
-    if (other.gameObject.name == "Stalker" && !isPlaying)
+    if (other.gameObject.name == "Stalker"/* && !isPlaying*/)
     {
       isPlaying = true;
-      if (!inEnd)
-        anim.clip = forwardClip;
-      else
-        anim.clip = backClip;
+      anim.clip = forwardClip;
       anim.Play();
       StartCoroutine(Finish(anim.clip.length));
     }
-    
 	}
+
+  private void OnTriggerExit(Collider other)
+  {
+    if (other.gameObject.name == "Stalker"/* && !isPlaying*/)
+    {
+      isPlaying = true;
+      anim.clip = backClip;
+      anim.Play();
+      StartCoroutine(Finish(anim.clip.length));
+    }
+  }
   //
   private IEnumerator Finish(float time)
   {
     yield return new WaitForSeconds(time);
     isPlaying = false;
-    inEnd = !inEnd;
   }
 }
