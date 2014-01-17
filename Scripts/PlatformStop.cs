bece12dc1392cd72a15e74ee5212b25f2bf3594e
BoberUnity <borisberez@rambler.ru>
2014-01-17 19:10:13 +0400

    Platforma

---
+++ Scripts/PlatformStop.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformStop : MonoBehaviour
+{
+  private Transform characterTransform = null;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.name == "Stalker")
+    {
+      characterTransform = other.transform;
+      if (characterTransform.position.y > 0.6f)
+      {
+        animation.Stop();
+        StartCoroutine(AgainMove(4));
+      }
+    }
+  }
+  //Снова запустим платформу, ежели перс внизу
+  private IEnumerator AgainMove(float time)
+  {
+    yield return new WaitForSeconds(time);
+    if (characterTransform.position.y > 0.6f)
+      StartCoroutine(AgainMove(4));
+    else 
+      animation.Play();
+  }
+}
