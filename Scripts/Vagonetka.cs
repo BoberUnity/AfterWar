9ea6efd2119d580fa3541f95437b3bbe9b242929
BoberUnity <borisberez@rambler.ru>
2014-01-29 16:01:21 +0400

    Crash vagonetka

--- Scripts/Vagonetka.cs
+++ Scripts/Vagonetka.cs
@@ -1,30 +1,38 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Vagonetka : MonoBehaviour
 {
   [SerializeField] private float speed = 1;
-  //[SerializeField] private Transform hero = null;
+  [SerializeField] private Transform stalker = null;
+  //[SerializeField] private BoxCollider boxCollider = null;
+  [SerializeField] private AudioClip crash = null;
+  [SerializeField] private GameObject kolesoR1 = null;
+  [SerializeField] private GameObject kolesoR2 = null;
+  [SerializeField] private GameObject kolesoL1 = null;
+  [SerializeField] private GameObject kolesoL2 = null;
   private float currSpeed = 0;
-  [SerializeField]
   private bool run = false;
   private bool dead = false;
+
+  
 
   private void OnTriggerEnter(Collider other)
   {
     if (other.gameObject.name == "Stalker")
       run = true;
-    if (other.gameObject.name == "Vrata")
+    if (other.gameObject.name == "Vrata" || other.gameObject.name == "BochkaBenz")
     {
-      run = false;
-      currSpeed = 0;
-      speed = 0;
+      Crash();
     }
 
-    if (other.gameObject.name == "Dyra")
+    if (other.gameObject.name == "Dyra" && !dead)
     {
       //hero.parent = null;
       //rigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
       dead = true;
+      audio.clip = crash;
+      audio.Play();
     }
   }
 
@@ -37,4 +45,27 @@ public class Vagonetka : MonoBehaviour
       transform.Translate(currSpeed*Time.deltaTime,0,0);
 	  }
 	}
+
+  private void Crash()
+  {
+    run = false;
+    currSpeed = 0;
+    speed = 0;
+    stalker.parent = null;
+    transform.position = new Vector3(transform.position.x, 0.17f, transform.position.z);
+    kolesoR1.AddComponent("BoxCollider");
+    kolesoR1.AddComponent("Rigidbody");
+    kolesoR1.rigidbody.AddForce(50, 50, -50);
+    kolesoR2.AddComponent("BoxCollider");
+    kolesoR2.AddComponent("Rigidbody");
+    kolesoR2.rigidbody.AddForce(50, 50, -40);
+    kolesoL1.AddComponent("BoxCollider");
+    kolesoL1.AddComponent("Rigidbody");
+    kolesoL1.rigidbody.AddForce(50, 50, 40);
+    kolesoL2.AddComponent("BoxCollider");
+    kolesoL2.AddComponent("Rigidbody");
+    kolesoL2.rigidbody.AddForce(50, 50, 30);
+    audio.clip = crash;
+    audio.Play();
+  }
 }
