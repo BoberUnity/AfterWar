c2a87e1ede2d1994743486ea4bb0519885596f03
BoberUnity <borisberez@rambler.ru>
2014-02-01 13:38:53 +0400

    Свет от гены

---
+++ Scripts/Buttons3D/WallLighttOn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Buttons3D
+{
+  public class WallLighttOn : Button3DBase
+  {
+    [SerializeField] private WallLight[] lamps = null;
+
+    protected override void MakeAction()
+    {
+      foreach (var l in lamps)
+      {
+        l.On = true;
+      }
+    }
+  }
+}
---
+++ Scripts/WallLight.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class WallLight : MonoBehaviour
+{
+  [SerializeField] private Material lightMaterial = null;
+  [SerializeField] private float speed = 1;
+  [SerializeField] private float maxA = 1;
+  [SerializeField] private float minA = 0;
+  private bool on = false;
+  private bool up = false;
+  private float a = 0;
+
+  public bool On
+  {
+    set { on = value;}
+  }
+
+	void Start () 
+  {
+    lightMaterial.color = new Color(1, 1, 0.5f, minA);
+	}
+	
+	void Update () 
+  {
+    if (on)
+    {
+      if (up)
+      {
+        a += Time.deltaTime*speed;
+        if (a > maxA)
+        {
+          up = false;
+          a = maxA;
+        }
+      }
+      else
+      {
+        a -= Time.deltaTime*speed;
+        if (a < minA)
+        {
+          up = true;
+          a = minA;
+        }
+      }
+      lightMaterial.color = new Color(1, 1, 0.5f, a);
+    }
+  }
+}
