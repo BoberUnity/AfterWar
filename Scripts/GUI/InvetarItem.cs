28380d8a5b315895bb61ca33cf2cbc4853bfbb9c
BoberUnity <borisberez@rambler.ru>
2014-02-05 20:26:28 +0400

    Рычаги на 3 уровне

---
+++ Scripts/GUI/InvetarItem.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class InvetarItem : MonoBehaviour 
+{
+  protected virtual void OnPress(bool isPressed)
+  {
+    if (!isPressed)
+      gameObject.SetActive(false);
+  }
+}
