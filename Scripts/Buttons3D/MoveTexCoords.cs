77c6f835a9e4036964bbf2f920134fd4b1e8b817
BoberUnity <borisberez@rambler.ru>
2014-02-11 21:43:32 +0400

    Lev7

---
+++ Scripts/Buttons3D/MoveTexCoords.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MoveTexCoords : Button3DBase
+{
+  [SerializeField] private Material mat = null;
+  [SerializeField] private float speed = 1;
+  private Vector2 offset = Vector2.zero;
+  private bool on = false;
+  //public bool On
+  //{
+  //  set { on = value; }
+  //}
+
+  protected override void MakeAction()
+  {
+    on = true;
+  }
+
+  void Update()
+  {
+    if (on)
+    {
+      offset.y += Time.deltaTime * speed;
+      mat.SetTextureOffset("_MainTex", offset);
+    }
+  }
+}
