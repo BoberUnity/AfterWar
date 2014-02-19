893718e0b6e4bfe83ed9a710b071a3b0c940d4b3
BoberUnity <borisberez@rambler.ru>
2014-02-19 22:01:32 +0400

    Task2

---
+++ Scripts/Buttons3D/PlayTwoAnims.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Buttons3D
+{
+  public class PlayTwoAnims : Button3DBase
+  {
+    [SerializeField] private int id = 0;
+    [SerializeField] private Animation anim = null;
+    [SerializeField] private AnimationClip clipFirst = null;
+    [SerializeField] private AnimationClip clipSecond = null;
+    [SerializeField] private AudioClip sound = null;
+    public event Action<int, bool> Press = null;
+
+    private bool forward = true;
+    public bool Forward
+    {
+      get { return forward;}
+    }
+
+    public int Id
+    {
+      get { return id;}
+    }
+    
+    private bool isPlayed = false;
+
+    protected override void MakeAction()
+    {
+      if (!isPlayed)
+      {
+        if (forward)
+          anim.clip = clipFirst;
+        else
+          anim.clip = clipSecond;
+        forward = !forward;
+        anim.Play();
+        PlayingSound();
+        StartCoroutine(EndAnim(anim[anim.clip.name].length));
+        
+        isPlayed = true;
+        var handled = Press;
+        if (handled != null)
+          handled(id, forward);
+      }
+    }
+
+    private IEnumerator EndAnim(float time)
+    {
+      yield return new WaitForSeconds(time);
+      if (sound != null)
+        audio.Stop();
+      isPlayed = false;
+    }
+
+    private void PlayingSound()
+    {
+      if (sound != null)
+      {
+        audio.clip = sound;
+        if (character.Controller != null)
+          audio.volume = character.Controller.EffectsVolume;
+        //audio.loop = loop;
+        audio.Play();
+        
+      }
+    }
+  }
+}
---
+++ Scripts/DoorKey.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using Assets.Scripts.Buttons3D;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+  [Serializable] private class Block
+  {
+    [SerializeField] public PlayTwoAnims playTwoAnimses = null;
+    [SerializeField] public bool key = false;
+  }
+
+  [SerializeField] private Animation doorAnim = null;
+  [SerializeField] private Renderer lampRenderer = null;
+  [SerializeField] private Material grayMat = null;
+  [SerializeField] private Material redMat = null;
+  [SerializeField] private Material greenMat = null;
+  [SerializeField] private GameObject redFlare = null;
+  [SerializeField] private GameObject greenFlare = null;
+  [SerializeField] private Block[] blocks = null;
+  private bool isOpen = false;
+
+	private void Start () 
+  {
+    foreach (var but in blocks)
+    {
+      but.playTwoAnimses.Press += CheckKey;
+    }
+	}
+
+  private void OnDestroy()
+  {
+    foreach (var but in blocks)
+    {
+      but.playTwoAnimses.Press -= CheckKey;
+    }
+  }
+	
+	private void CheckKey(int id, bool f)
+	{
+	  if (!isOpen)
+    {
+      bool right = true;
+
+      foreach (var but in blocks)
+	    {
+        if (but.playTwoAnimses.Forward != but.key)
+          right = false;
+	    }
+      if (right)
+      {
+        isOpen = true;
+        doorAnim.Play();
+        lampRenderer.material = greenMat;
+        greenFlare.gameObject.SetActive(true);
+      }
+      else
+      {
+        lampRenderer.material = redMat;
+        redFlare.gameObject.SetActive(true);
+        StartCoroutine(ReturnMat(0.7f));
+      }
+    }
+  }
+
+  private IEnumerator ReturnMat(float time)
+  {
+    yield return new WaitForSeconds(time);
+    lampRenderer.material = grayMat;
+    redFlare.gameObject.SetActive(false);
+  }
+}
